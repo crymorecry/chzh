b8fdc69562032774de15297ca10dc074463c6fd4
Андрей Кочуров <andrejkocurov@Noutbuk-Andrej.local>
2024-10-16 19:06:56 +0300

    photo redact

--- components/audit/page.tsx
+++ components/audit/page.tsx
@@ -5,10 +5,10 @@ export default function Audit() {
         <div className="h-full bg-gradient-to-b from-purple-500 to-indigo-600 py-8 lg:py-20 w-full">
             <div className="flex flex-col gap-y-8 w-11/12 lg:w-11/12 2xl:w-10/12 mx-auto">
                 <span className="text-slate-100 font-bold text-2xl lg:text-5xl max-w-7xl">ПАРУ СЛОВ ОБ АУДИТОРИИ</span>
-                <div className="lg:flex lg:flex-row grid grid-cols-3 justify-between w-full gap-4">
-                    <a href="/audit/1.jpg"><Image src="/audit/1.jpg" width={2000} height={2000} className="rounded-xl lg:h-[900px] h-full w-full object-cover max-w-lg" alt="/" /></a>
-                    <a href="/audit/2.jpg"><Image src="/audit/2.jpg" width={2000} height={2000} className="rounded-xl lg:h-[900px] h-full w-full object-cover max-w-lg" alt="/" /></a>
-                    <a href="/audit/3.jpg"><Image src="/audit/3.jpg" width={2000} height={2000} className="rounded-xl lg:h-[900px] h-full w-full object-cover max-w-lg" alt="/" /></a>
+                <div className="lg:flex lg:flex-row grid grid-cols-3 justify-between w-full gap-4 lg:h-[1000px]">
+                    <a href="/audit/1.jpg" className="w-full flex"><Image src="/audit/1.jpg" width={2000} height={2000} className="rounded-xl h-full w-full max-w-lg" alt="/" /></a>
+                    <a href="/audit/2.jpg" className="w-full flex"><Image src="/audit/2.jpg" width={2000} height={2000} className="rounded-xl h-full w-full max-w-lg" alt="/" /></a>
+                    <a href="/audit/3.jpg" className="w-full flex"><Image src="/audit/3.jpg" width={2000} height={2000} className="rounded-xl h-full w-full max-w-lg" alt="/" /></a>
                 </div>
             </div>
         </div>
